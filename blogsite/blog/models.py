7befc0c692fc9525d255110637facf8337e97c7d
aravinds428 <aravinds428@gmail.com>
2020-05-10 10:18:39 +0530

    changed the editor

--- blogsite/blog/models.py
+++ blogsite/blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from ckeditor.fields import RichTextField
 # Create your models here.
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField()
+    #content = models.TextField()
+    content = RichTextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
